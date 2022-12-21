b3a42bb9fa1fa905a2ba032e4309f4320b3c5d70
MarkZither <mark.burton@zither-it.co.uk>
2022-12-21 21:58:11 +0100

    first compiling version

---
+++ src/NukeBuildExtensions.AzurePipelines/AzurePipelinesExtendedAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using Nuke.Common.Execution;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Utilities;
+
+using NukeBuildExtensions.AzurePipelines.Configuration;
+using Nuke.Common.CI.AzurePipelines.Configuration;
+
+namespace NukeBuildExtensions.AzurePipelines
+{
+    public class AzurePipelinesExtendedAttribute : AzurePipelinesAttribute
+    {
+        public AzurePipelinesExtendedAttribute(string suffix, AzurePipelinesImage image, params AzurePipelinesImage[] images) : base(suffix, image, images)
+        {
+        }
+
+        public bool NuGetAuthenticate { get; set; }
+        protected override IEnumerable<AzurePipelinesStep> GetSteps(ExecutableTarget executableTarget, IReadOnlyCollection<ExecutableTarget> relevantTargets, AzurePipelinesImage image)
+        {
+            if (NuGetAuthenticate)
+            {
+                yield return new AzurePipelinesNuGetAuthenticateStep();
+            }
+
+            foreach (var step in base.GetSteps(executableTarget, relevantTargets, image))
+            {
+                yield return step;
+            }
+        }
+        //protected override IEnumerable<AzurePipelinesStep> GetSteps(ExecutableTarget executableTarget, IReadOnlyCollection<ExecutableTarget> relevantTargets)
+          //  => base.GetSteps(executableTarget, relevantTargets).Prepend(new AzurePipelinesNuGetAuthenticateStep());
+    }
+}
---
+++ src/NukeBuildExtensions.AzurePipelines/Configuration/AzurePipelinesNuGetAuthenticateStep.cs
@@ -0,0 +1,10 @@
+using Nuke.Common.CI.AzurePipelines.Configuration;
+using Nuke.Common.Utilities;
+
+namespace NukeBuildExtensions.AzurePipelines.Configuration
+{
+    public class AzurePipelinesNuGetAuthenticateStep : AzurePipelinesStep
+    {
+        public override void Write(CustomFileWriter writer) => writer.WriteLine("- task: NuGetAuthenticate@0");
+    }
+}
