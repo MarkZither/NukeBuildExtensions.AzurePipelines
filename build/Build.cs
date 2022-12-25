30ae4bfdf7cce2356e9fdb5a4dff7d91abe857ec
MarkZither <mark.burton@zither-it.co.uk>
2022-12-25 22:07:02 +0100

    implement build restore target

--- build/Build.cs
+++ build/Build.cs
@@ -6,11 +6,13 @@ using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
     "continuous",
@@ -36,6 +38,8 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -45,6 +49,8 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
