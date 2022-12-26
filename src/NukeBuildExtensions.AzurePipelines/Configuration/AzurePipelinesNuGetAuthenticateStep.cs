f31084b86aa16f533ecc5f59481bf31071516838
MarkZither <mark.burton@zither-it.co.uk>
2022-12-26 14:39:14 +0100

    use NuGetAuthenticate@1 as 0 is deprecated

--- src/NukeBuildExtensions.AzurePipelines/Configuration/AzurePipelinesNuGetAuthenticateStep.cs
+++ src/NukeBuildExtensions.AzurePipelines/Configuration/AzurePipelinesNuGetAuthenticateStep.cs
@@ -5,6 +5,6 @@ namespace NukeBuildExtensions.AzurePipelines.Configuration
 {
     public class AzurePipelinesNuGetAuthenticateStep : AzurePipelinesStep
     {
-        public override void Write(CustomFileWriter writer) => writer.WriteLine("- task: NuGetAuthenticate@0");
+        public override void Write(CustomFileWriter writer) => writer.WriteLine("- task: NuGetAuthenticate@1");
     }
 }
